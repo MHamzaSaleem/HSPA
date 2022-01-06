c24b636bf4f18567875e160024011cf9a3eec4cd
M Hamza Saleem <mhamzasaleem3@gmail.com>
2022-01-06 15:58:04 +0500

    V.0.5

--- Frontend/src/app/user/user-register/user-register.component.ts
+++ Frontend/src/app/user/user-register/user-register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, RequiredValidator, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
-
+import { AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'user-register',
@@ -11,15 +11,37 @@ import { Router } from "@angular/router";
 
 )
 export class UserRegisterComponent implements OnInit {
-registrationForm: FormGroup;
-constructor (private router: Router){}
-ngOnInit(): void {
-  this.registrationForm = new FormGroup({
-    userName: new FormControl('Enter Name', Validators.required),
-    email: new FormControl(null,[Validators.required,Validators.email])
-  })
-}
-onSubmit(){
-  console.log(this.registrationForm)
-}
+  registrationForm: FormGroup;
+  constructor (private router: Router){}
+  ngOnInit(): void {
+    this.registrationForm = new FormGroup({
+      userName: new FormControl('', Validators.required),
+      email: new FormControl(null,[Validators.required,Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      confirmPassword: new FormControl(null, [Validators.required]),
+      mobile: new FormControl(null, [Validators.required, Validators.maxLength(10)])
+    },this.passwordMatchingValidator
+  )}
+
+  //custom cross validator for password matching
+  passwordMatchingValidator(fg: AbstractControl){
+    return fg.get('password')?.value === fg.get('confirmPassword')?.value ? null : {notmatched: true}
+  }
+
+  //getter method in angular it must return a value and cannot have parameters
+  get username(){
+    return this.registrationForm.get('userName') as FormControl;
+  }
+
+  get email(){
+    return this.registrationForm.get('email') as FormControl;
+  }
+
+  get password(){
+    return this.registrationForm.get('password') as FormControl;
+  }
+
+  onSubmit(){
+    console.log(this.registrationForm)
+  }
 }
